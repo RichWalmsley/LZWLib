52c12fb50f99b416cf397c1e99e7052d355e6646
RichWalmsley <r.walmsley97@gmail.com>
2025-11-25 23:38:42 -0330

    added some comments for scaffolding before I dive into this

--- src/LZWLib.hs
+++ src/LZWLib.hs
@@ -1,9 +1,21 @@
 module LZWLib (compressLZW, decompressLZW) where
 
+-- compression algorithm
+
 compressLZW :: Eq a => [a] -> [a] -> [Int]
 compressLZW _ [] = []
-compressLZW as (c:cs) = 
+compressLZW as (c:cs) = undefined 
+
+-- compression helpers
+
+
+
+-- decompression algorithm
 
 decompressLZW :: [a] -> [Int] -> [a] 
 decompressLZW _ [] = []
-decompressLZW as xs =
+decompressLZW as xs = undefined
+
+-- decompression helpers
+
+
