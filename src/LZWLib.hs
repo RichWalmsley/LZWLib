da1479345149c9f14273de37fadbc0bc40325dfb
RichWalmsley <r.walmsley97@gmail.com>
2025-11-29 18:49:37 -0330

    Started compressLZW function

--- src/LZWLib.hs
+++ src/LZWLib.hs
@@ -1,21 +1,49 @@
-module LZWLib (compressLZW, decompressLZW) where
+module LZWLib (lzwCompress, lzwDecompress) where
+
+import qualified Data.Map.Strict as Map
+import qualified Data.IntMap.Strict as IntMap
+import Data.Char (chr, ord)
+import Data.Maybe (fromMaybe)
+
+
+initalDict :: Map.Map String Int
+initialDict = Map.fromList [ ([chr n], n) | n <- [0..255] ]
+
+revInitialDict :: IntMap.IntMap String
+revInittialDict = IntMap.fromList [ (n, [chr n]) | n <- [0..255] ]
 
 -- compression algorithm
 
-compressLZW :: Eq a => [a] -> [a] -> [Int]
+compressLZW :: String -> [Int]
 compressLZW _ [] = []
-compressLZW as (c:cs) = undefined 
-
--- compression helpers
-
-
+compressLZW input = compressLoop input "" initialDict 256 []
+    where
+        compressLoop
+            :: String               -- remaining input
+            -> String               -- current prefix (w)
+            -> Map.Map String Int   -- dict
+            -> Int                  -- next available code
+            -> [Int]                -- output codes reversed
+            -> [Int]                -- final code list
+        compressLoop [] "" _ _ acc = reverse acc
+        compressLoop [] w dict _ acc = reverse (lookupCode dict w : acc)
+        compressLoop (k:ks) w dict nextCode acc =
+            let wk = w ++ [k]
+            in if Map.member wk dict
+                then compressLoop ks wk dict nextCode acc
+                else
+                    let outCode = lookupCode dict w
+                        dict'   = Map.insert wk nextCode dict
+                        acc'    = outCode : acc
+                    in compressLoop ks [k] dict' (nextCodde + 1) acc'
 
 -- decompression algorithm
 
-decompressLZW :: [a] -> [Int] -> [a] 
-decompressLZW _ [] = []
-decompressLZW as xs = undefined
+lzwDecompress :: [Int] -> Maybe String
+lzwDecompress [] = Just ""
+lzwDecompress (firstCode : rest) = undefined
 
--- decompression helpers
+-- Utility
 
-
+lookupCode :: Map.Map String Int -> String -> Int
+lookupCode = undefined
