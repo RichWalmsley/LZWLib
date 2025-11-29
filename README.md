9aa02a165e8e254c46d24b24cb6ccca3fa8d4aeb
RichWalmsley <r.walmsley97@gmail.com>
2025-11-29 19:44:28 -0330

    Deleted everything to write from scratch

---
+++ README.md
@@ -0,0 +1,3 @@
+# LZW Lib
+
+Haskell implementation of the LZW compression algorithm. Done to learn about compression algorithms, practice and recreational programming.
--- src/LZWLib.hs
+++ src/LZWLib.hs
@@ -5,45 +5,24 @@ import qualified Data.IntMap.Strict as IntMap
 import Data.Char (chr, ord)
 import Data.Maybe (fromMaybe)
 
+-- Dictionary intializers
 
-initalDict :: Map.Map String Int
+initialDict :: Map.Map String Int
 initialDict = Map.fromList [ ([chr n], n) | n <- [0..255] ]
 
 revInitialDict :: IntMap.IntMap String
-revInittialDict = IntMap.fromList [ (n, [chr n]) | n <- [0..255] ]
+revInitialDict = IntMap.fromList [ (n, [chr n]) | n <- [0..255] ]
 
 -- compression algorithm
 
-compressLZW :: String -> [Int]
-compressLZW _ [] = []
-compressLZW input = compressLoop input "" initialDict 256 []
-    where
-        compressLoop
-            :: String               -- remaining input
-            -> String               -- current prefix (w)
-            -> Map.Map String Int   -- dict
-            -> Int                  -- next available code
-            -> [Int]                -- output codes reversed
-            -> [Int]                -- final code list
-        compressLoop [] "" _ _ acc = reverse acc
-        compressLoop [] w dict _ acc = reverse (lookupCode dict w : acc)
-        compressLoop (k:ks) w dict nextCode acc =
-            let wk = w ++ [k]
-            in if Map.member wk dict
-                then compressLoop ks wk dict nextCode acc
-                else
-                    let outCode = lookupCode dict w
-                        dict'   = Map.insert wk nextCode dict
-                        acc'    = outCode : acc
-                    in compressLoop ks [k] dict' (nextCodde + 1) acc'
-
+lzwCompress :: String -> [Int]
+lzwCompress input = undefined
+    
 -- decompression algorithm
 
 lzwDecompress :: [Int] -> Maybe String
 lzwDecompress [] = Just ""
 lzwDecompress (firstCode : rest) = undefined
 
--- Utility
+-- helpers
 
-lookupCode :: Map.Map String Int -> String -> Int
-lookupCode = undefined
